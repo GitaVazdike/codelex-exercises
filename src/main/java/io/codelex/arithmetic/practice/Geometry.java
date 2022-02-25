23d9e6fbd8a8900dac1dab398befcacdef87633d
Gita <gita.gulevska@inbox.lv>
2022-02-26 01:51:49 +0200

    Finished Arithmetic Exercise #10

--- src/main/java/io/codelex/arithmetic/practice/Geometry.java
+++ src/main/java/io/codelex/arithmetic/practice/Geometry.java
@@ -1,19 +1,37 @@
 package io.codelex.arithmetic.practice;
 
-import io.codelex.NoCodeWrittenException;
-
 import java.math.BigDecimal;
 
 class Geometry {
     static double areaOfCircle(BigDecimal radius) {
-        throw new NoCodeWrittenException();
+
+        if (radius.signum() == -1) {
+            throw new RuntimeException("Radius cannot be negative.");
+        }
+
+        BigDecimal circleArea = BigDecimal.valueOf(Math.PI).multiply(radius).multiply(BigDecimal.valueOf(2));
+        return circleArea.doubleValue();
     }
 
     static double areaOfRectangle(BigDecimal length, BigDecimal width) {
-        throw new NoCodeWrittenException();
+        if (length.signum() == -1) {
+            throw new RuntimeException("Length cannot be negative.");
+        } else if (width.signum() == -1) {
+            throw new RuntimeException("Width cannot be negative.");
+        }
+
+        BigDecimal rectangleArea = length.multiply(width);
+        return rectangleArea.doubleValue();
     }
 
     static double areaOfTriangle(BigDecimal base, BigDecimal h) {
-        throw new NoCodeWrittenException();
+        if (base.signum() == -1) {
+            throw new RuntimeException("Base cannot be negative.");
+        } else if (h.signum() == -1) {
+            throw new RuntimeException("Height cannot be negative.");
+        }
+
+        BigDecimal triangleArea = base.multiply(h).multiply(BigDecimal.valueOf(0.5));
+        return triangleArea.doubleValue();
     }
 }
