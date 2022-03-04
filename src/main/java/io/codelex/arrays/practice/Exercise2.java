c0ad274a5a81099c9101162c0c125c660e297fa7
Gita <gita.gulevska@inbox.lv>
2022-03-04 15:41:17 +0200

    Finished Exercises: Arrays 1 - 7.

--- src/main/java/io/codelex/arrays/practice/Exercise2.java
+++ src/main/java/io/codelex/arrays/practice/Exercise2.java
@@ -1,5 +1,6 @@
 package io.codelex.arrays.practice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise2 {
@@ -15,15 +16,16 @@ public class Exercise2 {
         System.out.println("Please enter a max number");
         int maxNumber = input.nextInt();
 
-        int myArray[];
+        int[] myArray = new int[maxNumber + 1];
         int sum = 0;
 
-        /*
-        fixme
-        for (........) {
-        ........
+        for (int i = minNumber; i <= maxNumber; i++) {
+            myArray[i] = i;
         }
-        */
+
+        for (int j = 0; j < myArray.length; j++) {
+            sum += myArray[j];
+        }
 
         System.out.println("The sum is " + sum);
     }
