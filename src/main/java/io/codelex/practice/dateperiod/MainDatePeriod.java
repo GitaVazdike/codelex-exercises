ce9c3941ffbab353df613f92f5daf466c22128da
Gita <gita.gulevska@inbox.lv>
2022-03-16 20:59:00 +0200

    Finished Exercises: Date and Time.

---
+++ src/main/java/io/codelex/practice/dateperiod/MainDatePeriod.java
@@ -0,0 +1,20 @@
+package io.codelex.practice.dateperiod;
+
+import java.time.LocalDate;
+
+public class MainDatePeriod {
+    public static void main(String[] args) {
+        LocalDate firstStart = LocalDate.of(2022, 1, 1);
+        LocalDate firstEnd = LocalDate.of(2022, 1, 14);
+        DatePeriod firstPeriod = new DatePeriod(firstStart, firstEnd);
+
+        LocalDate secondStart = LocalDate.of(2022, 1, 10);
+        LocalDate secondEnd = LocalDate.of(2022, 1, 15);
+        DatePeriod secondPeriod = new DatePeriod(secondStart, secondEnd);
+
+        DatePeriod overlap = firstPeriod.intersection(secondPeriod);
+
+        System.out.println("Overlap: " + overlap);
+    }
+}
+
