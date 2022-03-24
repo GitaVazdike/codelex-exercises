ae53e65bc961996e5d522f826d27405048d80e4a
Gita <gita.gulevska@inbox.lv>
2022-03-24 19:47:22 +0200

    Exercises finished: Collections 1-6.

--- src/main/java/io/codelex/collections/practice/WordCount.java
+++ src/main/java/io/codelex/collections/practice/WordCount.java
@@ -6,13 +6,41 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class WordCount {
     private static final Charset charset = Charset.defaultCharset();
     private static final String file = "/collections/lear.txt";
 
     public static void main(String[] args) throws IOException, URISyntaxException {
+
         final Path path = Paths.get(Histogram.class.getResource(file).toURI());
-        Files.readAllLines(path, charset);
+        List<String> lines = Files.readAllLines(path, charset);
+        long lineCount = Files.lines(path).count();
+
+        System.out.println("Lines = " + lineCount);
+        System.out.println("Words = " + getWordCount(lines));
+        System.out.println("Chars = " + getCharCount(lines));
     }
+
+    private static int getWordCount(List<String> textLines) {
+        int wordCount = 0;
+        for (String line : textLines) {
+            line = line.replace("'", " ").replace("  ", " ");
+            String[] words = line.split(" ");
+            wordCount += words.length;
+        }
+        return wordCount;
+    }
+
+    private static int getCharCount(List<String> textLines) {
+        int charCount = 0;
+        for (String line : textLines) {
+            for (int i = 0; i < line.length(); i++) {
+                charCount++;
+            }
+        }
+        return charCount;
+    }
+
 }
--- src/main/java/io/codelex/collections/practice/lists/ArrayListExercise11.java
+++ src/main/java/io/codelex/collections/practice/lists/ArrayListExercise11.java
@@ -1,21 +1,46 @@
 package io.codelex.collections.practice.lists;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ArrayListExercise11 {
 
     public static void main(String[] args) {
         //TODO: Create an ArrayList with String elements
+        ArrayList<String> groceryList = new ArrayList<>();
 
         //TODO: Add 10 values to list
+        groceryList.add("Bread");
+        groceryList.add("Cheese");
+        groceryList.add("Chocolate");
+        groceryList.add("Potatoes");
+        groceryList.add("Milk");
+        groceryList.add("Pasta");
+        groceryList.add("Tomatoes");
+        groceryList.add("Cereals");
+        groceryList.add("Bacon");
+        groceryList.add("Eggs");
 
         //TODO: Add new value at 5th position
+        groceryList.add(4, "Grapes");
 
         //TODO: Change value at last position (Calculate last position programmatically)
+        groceryList.set(groceryList.size() - 1, "Tomato sauce");
 
         //TODO: Sort your list in alphabetical order
+        Collections.sort(groceryList);
 
         //TODO: Check if your list contains "Foobar" element
+        if (groceryList.contains("Foobar")) {
+            System.out.println("Foobar found.");
+        } else {
+            System.out.println("Foobar not found.");
+        }
 
         //TODO: Print each element of list using loop
+        for (String element : groceryList) {
+            System.out.println(element);
+        }
     }
 
 }
